7b71a0549a6230d4ff53da64b7ed3e1b43728613
AlejandroSH1 <142269078+AlejandroSH1@users.noreply.github.com>
2024-10-15 22:43:57 -0600

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,21 +1,37 @@
+/*
+ * Agencia Vehicular
+ * A01711368
+ * Alfredo Alejandro Soto Herrera
+ * 14/10/24
+ * Esta archivo define la clase de nodo y de árbol BST, para poder guardar la 
+ * lectura del archivo en una estructura de árbol.
+ */
+
 #ifndef BST_H
 #define BST_H
 
 #include "vehiculo.h"
 #include <fstream>
 #include <sstream>
+#include <string>
 #include <vector>
 
+using namespace std;
+
+// Declaro la clase NodoBST
 class NodoBST {
+// Declaro las variables    
 private:
     Vehiculo vehiculo;
     NodoBST* izquierda; 
     NodoBST* derecha; 
+// Declaro los métodos de la clase
 public:
+    // Constructor
     NodoBST(Vehiculo v) : vehiculo(v), izquierda(nullptr), derecha(nullptr) {}
-
+    // Métodos
     NodoBST* insertar(Vehiculo v);
-    void inOrder();
+    void inOrder(vector<Vehiculo>&) const;
 };
 
 NodoBST* NodoBST::insertar(Vehiculo v) {
@@ -34,18 +50,16 @@ NodoBST* NodoBST::insertar(Vehiculo v) {
     }
     return this;
 }
-void NodoBST::inOrder() {
+
+void NodoBST::inOrder(vector<Vehiculo>& result) const {
     if (izquierda != nullptr) {
-        izquierda->inOrder(); 
-    }
-
-    vehiculo.mostrar_info();
-
-    if (derecha != nullptr) {
-        derecha->inOrder();
-    }
+            izquierda->inOrder(result);
+        }
+        result.push_back(vehiculo);
+        if (derecha != nullptr) {
+            derecha->inOrder(result);
+        }
 }
-
 
 class ArbolBST {
 private:
@@ -54,8 +68,7 @@ private:
 public:
     ArbolBST() : raiz(nullptr) {}
     void insertar(Vehiculo v);
-
-    void inOrder();
+    vector<Vehiculo> sortVe() const;
     void leerCSV(const string& nombreArchivo);
 };
 
@@ -67,12 +80,12 @@ void ArbolBST::insertar(Vehiculo v) {
     }
 }
 
-void ArbolBST::inOrder() {
-    if (raiz != nullptr) {
-        raiz->inOrder();
-    } else {
-        std::cout << "El árbol está vacío." << std::endl;
-    }
+vector<Vehiculo> ArbolBST :: sortVe() const {
+    vector<Vehiculo> result;
+        if (raiz != nullptr) {
+            raiz->inOrder(result);
+        }
+        return result;
 }
 
 void ArbolBST::leerCSV(const string& nombreArchivo) {
