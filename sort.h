6fb4597b6cdd7a126e85e736a84a08ecf8b3a7aa
AlejandroSH1 <142269078+AlejandroSH1@users.noreply.github.com>
2024-10-15 22:44:32 -0600

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,44 @@
+/*
+ * Agencia Vehicular
+ * A01711368
+ * Alfredo Alejandro Soto Herrera
+ * 14/10/24
+ * Esta archivo define la clase sort, la cual utiliza Bubble Sort para ordenar
+ * el vector de la clase vehículo.
+ */
+
+#ifndef SORT_H
+#define SORT_H
+
+#include "bst.h"
+#include "vehiculo.h"
+#include <iostream>
+#include <algorithm>
+
+// Declaro la clase Sort
+class Sort{
+public:
+/**
+  * bubbleSort(vector<Vehiculo>& vec) le aplica bubble sort al vector de los
+  * vehículos de menor a mayor.
+  *
+  * Utilizando el método de Bubble sort, se ordena el vector.
+  *
+  * Complejidad: O(n^2) en el peor de los casos.
+  * 
+  *@param vector
+  *@return void
+*/
+    void bubbleSort(vector<Vehiculo>& vec) {
+        int n = vec.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+            
+                if (vec[j].calcula_precio_venta() > vec[j + 1].calcula_precio_venta()) {
+                    swap(vec[j], vec[j + 1]); 
+                }
+            }
+        }
+    }
+};
+#endif
